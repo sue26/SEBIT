e093a1a167a45f713ba344d5f48fd11d0cf793c7
Soo Jung Bae <bae@Soos-MacBook-Pro.local>
2018-01-04 22:40:02 -0500

    first commit

---
+++ data.py
@@ -0,0 +1,27 @@
+def Articles():
+	articles = [
+		{
+			'id' : 1,
+			'title' : 'Article One',
+			'body' : 'this is body',
+			'author' : 'me',
+			'create_date': '04-25-2017'
+		},
+		{
+			'id' : 2,
+			'title' : 'Article Two',
+			'body' : 'this is body',
+			'author' : 'me2',
+			'create_date': '04-25-2017'
+		},
+		{
+			'id' : 3,
+			'title' : 'Article Three',
+			'body' : 'this is body',
+			'author' : 'me3', 
+			'create_date': '04-25-2017'
+		}
+
+	]
+
+	return articles
